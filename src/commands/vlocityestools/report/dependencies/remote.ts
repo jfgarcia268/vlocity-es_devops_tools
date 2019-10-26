28ad2f5da8d136b7b5f1b4e978672405b2e56c93
Juan <jgonzalez@vlocity.com>
2019-10-26 15:36:58 -0300

    Done Remote Dependencies

--- src/commands/vlocityestools/report/dependencies/remote.ts
+++ src/commands/vlocityestools/report/dependencies/remote.ts
@@ -5,20 +5,22 @@ import { AppUtils } from '../../../../utils/AppUtils';
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
 
-    //////// OmniScrits
-    var languageField;
-    var typeField;
-    var subTypeField;
-    var isActiveField;
-    var versionField;
-    var isProcedureField;
-    var isResusableField;
-    var elementsRelation;
-    var propertySetOSField;
-    //////// Element 
-    var elementPropertySetObject;
-    var elementOSIDField;
+//////// OmniScrits
+var languageField;
+var typeField;
+var subTypeField;
+var isActiveField;
+var versionField;
+var isProcedureField;
+var isResusableField;
+var propertySetOSField;
+//////// Element 
+var elementOSIDField;
 
+var totalDependenciesFound;
+var numberofOSandVIP;
+
+var resultsFile = './Dependencies_Report_Remote.csv'; 
 
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
@@ -54,6 +56,17 @@ export default class remote extends SfdxCommand {
   public async run(){
 
     const fs = require('fs');
+
+    totalDependenciesFound = 0;
+
+    AppUtils.log2('Results File: ' + resultsFile ); 
+    if (fs.existsSync(resultsFile)) {
+      fs.unlinkSync(resultsFile);
+    }
+
+    const CreateFiles = fs.createWriteStream(resultsFile, {flags: 'a'});
+    var initialHeader = 'DataPack Name,Is Reusable,Dependency,Dependency Type,Remote Class,Remote Method';
+    CreateFiles.write(initialHeader+'\r\n');   
 
     var packageType = this.flags.package;
     if(packageType == 'cmt'){
@@ -94,137 +107,104 @@ export default class remote extends SfdxCommand {
     oSValuesQuery = oSValuesQuery + ' WHERE ' + isActiveField + ' = true';
     var oSValuesQueryFinal = AppUtils.replaceaNameSpace(oSValuesQuery);
 
+    AppUtils.log3('Looking for OmniScripts and IntegrationProcedures in the environmemt');
     var oSValuesResults = await conn.query(oSValuesQueryFinal);
 
 
-    let oSValuesPap = new Map();
+    let oSValuesMap = new Map<string,Object>();
 
     for (var i=0; i<oSValuesResults.records.length; i++) {
       var element = oSValuesResults.records[i];
       //console.log('///////' + Object.keys(element) + ' ///' + element['Id'])
-      oSValuesPap.set(element['Id'], element); 
+      oSValuesMap.set(element['Id'], element); 
     }
 
-    ////// Create Maps
-    var initialQuery = AppUtils.replaceaNameSpace('SELECT %name-space%OmniScriptId__c , COUNT(Id) Total ');
-    initialQuery = initialQuery + AppUtils.replaceaNameSpace('FROM %name-space%Element__c ');
-    initialQuery = initialQuery + AppUtils.replaceaNameSpace('GROUP BY %name-space%OmniScriptId__c');
-    var queryString = AppUtils.replaceaNameSpace(initialQuery);
-
-    var resultFirst = await conn.query(queryString);
-
-    var currentCount = 0
-    var batches = new Array<string>();
-    var currentBatch = '';
-    for (var i=0; i<resultFirst.records.length; i++) {
-      var record = resultFirst.records[i];
-      var numberOfElements = record['Total'];
-      var dataPackId = record[AppUtils.replaceaNameSpace('%name-space%OmniScriptId__c')];
-
-      var dataPackRecord = oSValuesPap.get(dataPackId);
-      var shouldBeIncluded = (oSValuesPap.get(dataPackId)!= undefined);
-
-      //console.log('//////shouldBeIncluded: ' + shouldBeIncluded);
-
-      if((i == (+resultFirst.records.length - 1)) && ((currentCount+numberOfElements)>10000) ){
-        batches.push(currentBatch);
-        if(shouldBeIncluded == true){
-          currentBatch = dataPackId;
-          currentCount = numberOfElements;
-          batches.push(currentBatch);
-        } 
-      }else if((i == (+resultFirst.records.length - 1)) && ((currentCount+numberOfElements)<10000) ){
-        if(shouldBeIncluded == true){
-          currentBatch = currentBatch + ', ' + dataPackId;
-          currentCount =  currentCount + numberOfElements;
-        }
-        batches.push(currentBatch);
-      }
-      else {
-        if(shouldBeIncluded == true ){
-          if(currentBatch != ''){
-            currentBatch = currentBatch + ', ' + dataPackId;
-            currentCount =  currentCount + numberOfElements;
-          } else { 
-            currentBatch =  dataPackId;
-            currentCount =  numberOfElements;
-          }
-        } else {
-          currentBatch = '';
-          currentCount = 0;
-        }
-      }
-
-    }
+    AppUtils.log3('OmniScripts and IntegrationProcedures Found: ' + oSValuesResults.records.length);
+    numberofOSandVIP = oSValuesResults.records.length;
 
     ////// 
+    AppUtils.log3('Looking for Dependencies in DataPacks');
+    remote.OmniScriptPropertySet(conn,fs,oSValuesResults,CreateFiles);    
+    AppUtils.log3('Looking for Dependencies in Elements');
+    remote.queryElements(conn,fs,oSValuesMap,CreateFiles);
 
-    remote.lauchBatches(conn, batches,fs);
+    //while(!done){};
+
+
 
   }
 
+  static report(){
+        ////////// Report
+        console.log('')
+        AppUtils.log3('Donde Finding Dependencies'); 
+        AppUtils.log3('Number of OmniScripts and IntegrationProcedures Scanned: ' + numberofOSandVIP); 
+        AppUtils.log3('Number of Total Dependencies Found: ' + totalDependenciesFound); 
+        AppUtils.log3('CSV File Generated: ' + resultsFile); 
+        console.log('')
+  }
 
-  static lauchBatches(conn,batches,fs){
-    var elementsQuery = 'SELECT id, %name-space%OmniScriptId__c, %name-space%PropertySet__c '
+  static OmniScriptPropertySet(conn,fs,omniScripRecords,CreateFiles){
+    var totalDep = 0;
+    omniScripRecords.records.forEach(element => {
+      var propertySet = element[propertySetOSField];
+      var omniScriptType = element[typeField];
+      var omniScriptSubType = element[subTypeField];
+      var omniScriptLanguage = element[languageField];
+      var dataPack = omniScriptType + '_' + omniScriptSubType + '_' + omniScriptLanguage;
+      var isReusableValue = element[isResusableField];
+      var dataPackType = 'OmniScript'
+      if(element[isProcedureField]==true){
+        var dataPackType = 'IntegrationProcedure'
+      }
+      var resultDep = remote.getPropertySetValues(CreateFiles,propertySet,dataPackType,dataPack,isReusableValue);
+      totalDep = totalDep + resultDep;
+    });
+
+    totalDependenciesFound = totalDependenciesFound + totalDep;
+  }
+
+
+  static queryElements(conn,fs,omniScripRecords,CreateFiles){
+
+    var elementsQuery = 'SELECT Id, Name, %name-space%OmniScriptId__c, %name-space%PropertySet__c '
     elementsQuery = elementsQuery +  'FROM %name-space%Element__c '
-
-    AppUtils.log3('Looking for OmniScripts and IntegrationProcedures in the environmemt'); 
+    var queryString2 = AppUtils.replaceaNameSpace(elementsQuery);
+    var totalDependenciesFound2 = 0; 
     
+    conn.bulk.query(queryString2)
+    .on('record', function(result) { 
+      //console.log('/////QUERY: ' +queryString2)
+      var elementPropertySet = result[propertySetOSField];
+      var omniScripId = result[elementOSIDField];
+      var omniScripRecord = omniScripRecords.get(omniScripId);
     
-    batches.forEach(batch => {
-
-      var queryWithIds = elementsQuery 
-      var queryString2 = AppUtils.replaceaNameSpace(queryWithIds);
-
-      console.log('/////QUERY: ' +queryString2)
-  
-      conn.bulk.query(queryString2)
-      .on('record', function(result) { 
-        if(result!=undefined){
-        console.log('/////RECORD: ' + Object.keys(result));
-        console.log(record[propertySetOSField]);
-        console.log('/////');
-        AppUtils.log3('Found: ' + result.records.length + ' Active OmniScripts and IntegrationProcedures'); 
-
-        var totalDependenciesFound = 0;
-
-        AppUtils.log3('Finding Dependencies...'); 
-
-        for (var i=0; i<result.records.length; i++) {
-          var record = result.records[i];
-
-          var omniScriptPropertySet = record[propertySetOSField];
-          if( omniScriptPropertySet != '') {
-            var resultDepPS = remote.getPropertySetValues(CreateFiles,omniScriptPropertySet,dataPackType,dataPack,isReusableValue);
-            totalDependenciesFound = totalDependenciesFound + resultDepPS;
-          }
-
-          var elements = record[elementsRelation].records;
-          var dataPackType = 'OmniScript'
-          if(record[isProcedureField]){
-            dataPackType = 'IntegrationProcedure'
-          }
-          
-          var omniScriptType = record[typeField];
-          var omniScriptSubType = record[subTypeField];
-          var omniScriptLanguage = record[languageField];
-          var dataPack = omniScriptType + '_' + omniScriptSubType + '_' + omniScriptLanguage;
-          var isReusableValue = record[isResusableField];
-
-          AppUtils.log2('Finding Dependencies for ' + dataPackType + ': ' + dataPack); 
-          for (var j=0; j<elements.length; j++) {
-            var propertySet = elements[j][elementPropertySetObject];
-            var resultDep = remote.getPropertySetValues(CreateFiles,propertySet,dataPackType,dataPack,isReusableValue);
-            totalDependenciesFound = totalDependenciesFound + resultDep;
-          }
+      if (omniScripRecord!=undefined){ 
+        var dataPackType = 'OmniScript'
+        if(omniScripRecord[isProcedureField]==true){
+          var dataPackType = 'IntegrationProcedure'
         }
-        }
-      })
-      .on('error', function(err) { 
-        console.error(err); 
-      });
-
-  });
+        var omniScriptType = omniScripRecord[typeField];
+        var omniScriptSubType = omniScripRecord[subTypeField];
+        var omniScriptLanguage = omniScripRecord[languageField];
+        var dataPack = omniScriptType + '_' + omniScriptSubType + '_' + omniScriptLanguage;
+        var isReusableValue = omniScripRecord[isResusableField];
+        AppUtils.log2('Looking for Dependencies in ' + dataPackType + ': ' + dataPack );
+        var resultDep = remote.getPropertySetValues(CreateFiles,elementPropertySet,dataPackType,dataPack,isReusableValue);
+        totalDependenciesFound = totalDependenciesFound + resultDep;
+      }
+    })
+    .on("queue", function(batchInfo) {
+      AppUtils.log3('Done looking for Dependencies in Elements');
+      console.log('queue, batchInfo:', batchInfo);
+    })
+    .on("end", function() {
+      remote.report();
+    })
+    .on('error', function(err) { 
+      console.error(err); 
+    })
+    totalDependenciesFound = totalDependenciesFound + totalDependenciesFound2;
   }
 
 
