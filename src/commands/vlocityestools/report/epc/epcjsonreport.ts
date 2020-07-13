1528e38887181f88325d8a6aff446fc02ba88633
Juan <jgonzalez@vlocity.com>
2020-07-13 13:20:09 -0300

    Test

--- src/commands/vlocityestools/report/epc/epcjsonreport.ts
+++ src/commands/vlocityestools/report/epc/epcjsonreport.ts
@@ -59,15 +59,50 @@ export default class epcJsonExport extends SfdxCommand {
 
       AppUtils.log3( "EPC Json Report: " + 'Starting Report');
 
+      var AAHeader = 'Id,ObjectId,AttributeId,ruleType,ruleExpression';
+      var AAFields = `{ 
+        {
+          "FieldName" : "Id", 
+          "Callback" : null
+        },
+        {
+          "ObjectId" : "%name-space%ObjectId__c", 
+          "Callback" : null
+        },
+        {
+          "AttributeId" : "%name-space%AttributeId__c", 
+          "Callback" : null
+        },
+        {
+          "AttributeId" : "%name-space%RuleData__c", 
+          "Fields" : [
+            {
+              "Name" : "expression"
+            },
+            {
+              "Name" : "ruleType"
+            }
+          ]
+        }
+        
+
+      }`
+
       var resultAAFile = 'AAResults.csv';
-      var resultAA = await epcJsonExport.attributeAssigment(conn,resultAAFile);
-      var resultProduct2File = 'Product2Results.csv';
-      var resultProduct2 = await epcJsonExport.product2(conn,resultProduct2File);
+      var resultAA = await epcJsonExport.ExportObject(conn, resultAAFile, '%name-space%AttributeAssignment__c',AAHeader);
+
+
+      //var resultAAFile = 'AAResults.csv';
+      //var resultAA = await epcJsonExport.attributeAssigment(conn,resultAAFile);
+
+
+      //var resultProduct2File = 'Product2Results.csv';
+      //var resultProduct2 = await epcJsonExport.product2(conn,resultProduct2File);
 
 
       AppUtils.log3('Final Report:');
       AppUtils.log3(resultAA + ' File: ' + resultAAFile);
-      AppUtils.log3(resultProduct2 + ' File: ' + resultProduct2File);
+      //AppUtils.log3(resultProduct2 + ' File: ' + resultProduct2File);
 
     } catch (e) {
         console.log(e); 
@@ -224,6 +259,82 @@ export default class epcJsonExport extends SfdxCommand {
       var value = await promise;
       AppUtils.stopSpinnerMessage('Done, ' + cont + ' Exported');
       return value;
+  }
+
+  static async ExportObject(conn, resultsFile, Object, header) {
+
+    var splitChararter = ','
+    var objectAPIName = AppUtils.replaceaNameSpace(Object);
+
+    AppUtils.log3( objectAPIName + ' Report, File: ' + resultsFile);
+
+    if (fsExtra.existsSync(resultsFile)) {
+      fsExtra.unlinkSync(resultsFile);
+    }
+
+    AppUtils.startSpinner('Exporting ' + objectAPIName);
+
+    const createFiles = fsExtra.createWriteStream(resultsFile, {flags: 'a'});
+    createFiles.write(header+'\r\n');  
+
+    var objectIdF = AppUtils.replaceaNameSpace('%name-space%ObjectId__c');
+    var attributeIdF = AppUtils.replaceaNameSpace('%name-space%AttributeId__c');
+    var ruleDataF= AppUtils.replaceaNameSpace('%name-space%RuleData__c');
+
+    var queryString= 'SELECT Id, '
+                   + objectIdF + ', '
+                   + attributeIdF + ', '
+                   + ruleDataF +  ' '
+                   + 'FROM %name-space%AttributeAssignment__c';
+    var queryString2 = AppUtils.replaceaNameSpace(queryString);
+
+    //console.log('queryString: ' + queryString2);
+
+    var cont = 0;
+
+    let promise = new Promise((resolve, reject) => {
+        conn.query(queryString2)
+        .on('record',  function(result) { 
+        cont ++;
+        var Id = result['Id'];
+        var objectId = result[objectIdF];
+        var attributeId = result[attributeIdF];
+        var ruleDataJson = result[ruleDataF];
+
+        //AppUtils.log1('Exporting AttributeAssigment: ' + Id);
+
+        if(ruleDataJson != undefined && ruleDataJson != "" && ruleDataJson != "[]" ) {
+            var ruleData = JSON.parse(ruleDataJson);
+            for( var i = 0 ; i < ruleData.length ; i++){
+                var rule = ruleData[i];
+                var ruleExpression = rule['expression'];
+                var ruleType = rule['ruleType'];
+                //console.log('ruleExpression: ' + ruleExpression + '  ruleType: ' + ruleType);
+                var newLine = Id + splitChararter + objectId + splitChararter + attributeId + splitChararter + ruleType + splitChararter + ruleExpression;
+                createFiles.write(newLine+'\r\n');  
+            }
+        } else {
+            var newLine = Id + splitChararter + objectId + splitChararter + attributeId;
+            createFiles.write(newLine+'\r\n');    
+        }
+        })
+        .on("queue",  function(batchInfo) {
+            AppUtils.log2( 'AttributeAssigment - queue' );
+        })
+        .on("end",  function() {
+            AppUtils.log2( 'AttributeAssigment - Report Done');
+            resolve('Number of AttributeAssigment: ' + cont);
+        })
+        .on('error',  function(err) { 
+            AppUtils.log2( 'AttributeAssigment - Report Error');
+            reject('AttributeAssigment - Error: ' + err );
+        })
+        .run({ autoFetch : true, maxFetch : 1000000 });     
+    });
+
+    var value = await promise;
+    AppUtils.stopSpinnerMessage('Done, ' + cont + ' Exported');
+    return value;
   }
     
 }
