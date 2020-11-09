fc60c069d97cc4700a493a64b3c80c8b6abdc1b7
Juan Felipe <jgonzalez@vlocity.com>
2020-11-09 10:18:22 -0300

    Clean Objects, Use DBUtils class

--- src/utils/DBUtils.ts
+++ src/utils/DBUtils.ts
@@ -1,5 +1,7 @@
 import { AppUtils } from './AppUtils';
 
+const fsExtra = require("fs-extra");
+
 export class DBUtils  {
 
     private static batchSize = 10000;
@@ -101,6 +103,87 @@ export class DBUtils  {
           AppUtils.log2('Error Creating  batches - Error: ' + error);
           //resultData.push({ ObjectName: objectName , RecordsFound: records.length , DeleteSuccess: 'No Error: ' + error});
         }
+      }
+
+      static async bulkAPIdelete(records,conn,objectName,save,hardelete,resultData,bulkApiPollTimeout) {
+        var deleteType = hardelete == true ? 'hardDelete' : 'Delete';
+        var job = await conn.bulk.createJob(objectName,deleteType);
+        await job.open();
+        //console.log(job);
+        var numOfComonents = records.length;
+        var div = numOfComonents/this.batchSize;
+        var numberOfBatches = Math.floor(div) == div ? div : Math.floor(div)  + 1;
+        var numberOfBatchesDone = 0;
+        AppUtils.log2('Number Of Batches to be created to delete Rows: ' + numberOfBatches);
+        try {
+          var promises = [];
+          for (var i=0; i<numberOfBatches; i++) {
+            
+            var arraytoDelete = records;
+            if(i<(numberOfBatches-1)) {
+              arraytoDelete = records.splice(0,this.batchSize);
+            }
+            let newp = new Promise((resolve, reject) => {
+              var batchNumber = i + 1;
+              var batch = job.createBatch()
+              AppUtils.log1('Creating Batch # ' + batchNumber + ' Number of Records: ' + arraytoDelete.length);
+    
+              //console.log('Enter Promise');
+              batch.execute(arraytoDelete)
+              .on("error",  function(err) { 
+                console.log('Error, batch Info:', err);
+                numberOfBatchesDone = numberOfBatchesDone +1;
+                if(resultData){
+                  resultData.push({ ObjectName: objectName , RecordsFound: records.length , DeleteSuccess: 'No Error: ' + err});
+                }
+                resolve();
+              })
+              .on("queue",  function(batchInfo) { 
+                batch.poll(5*1000 /* interval(ms) */, 1000*60*bulkApiPollTimeout /* timeout(ms) */);
+                AppUtils.log1('Batch #' + batchNumber +' with Id: ' + batch.id + ' Has started');
+              })
+              .on("response",  function(rets) { 
+                numberOfBatchesDone = numberOfBatchesDone +1;
+                var hadErrors = this.noErrors(rets);
+                //console.log(rets);
+                AppUtils.log1('Batch #' + batchNumber + ' With Id: ' + batch.id + ' Finished - Success: ' + hadErrors + '  '+ numberOfBatchesDone + '/' + numberOfBatches + ' Batches have finished');
+                if(resultData){
+                  resultData.push({ ObjectName: objectName , RecordsFound: records.length , DeleteSuccess: hadErrors});
+                }
+                  if(save){
+                  DBUtils.saveResults(rets,batchNumber,objectName);
+                }
+                resolve();
+              });
+              //console.log('batch: '+ batch);
+            }).catch(error => {
+              AppUtils.log2('Error Creating  batches - Error: ' + error);
+              if(resultData){
+                resultData.push({ ObjectName: objectName , RecordsFound: records.length , DeleteSuccess: 'No Error: ' + error});
+              }
+            });
+            await promises.push(newp);
+          }
+          //console.log('Promise Size: '+ Promise.length);
+          await Promise.all(promises);
+          job.close();
+        } catch (error) {
+          job.close();
+          AppUtils.log2('Error Creating  batches - Error: ' + error);
+          if(resultData){
+            resultData.push({ ObjectName: objectName , RecordsFound: records.length , DeleteSuccess: 'No Error: ' + error});
+          }
+        }
+      }
+
+      private static saveResults(rets,batchNumber,objectName) {
+        var fileName = 'Results_' + objectName + '_' + batchNumber + '.json';
+        if (fsExtra.existsSync(fileName)) {
+          fsExtra.unlinkSync(fileName);
+        }
+        const createFiles = fsExtra.createWriteStream(fileName, {flags: 'a'});
+        createFiles.write(JSON.stringify(rets));  
+        AppUtils.log1('File Created: ' + fileName); 
       }
 
       private static noErrors(rets){
